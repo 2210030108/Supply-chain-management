4058c4be2ce2b7723dba5d21c936f32703cb9384
2210030108 <2210030108@klh.edu.in>
2024-08-30 10:17:11 +0530

    Remove all files from repository

--- spring-boot-backend/src/main/java/com/example/one/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.one.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
